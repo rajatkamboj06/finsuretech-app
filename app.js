fd74de295237b3e04def121ecf3f1003dfb86f76
Rajat Kamboj <142865956+rajatkamboj06@users.noreply.github.com>
2025-06-30 09:09:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const port = 3001;
 app.get('/', (req, res) => {
   res.send('Welcome to FinSureTech Web App!');
 });
-app.get('/', (req, res) => {
-  res.send('We are Group 5');
-});
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`);
