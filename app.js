5bfec00d7b45b92c9a03e9be38d9dc4191e23951
Rajat Kamboj <kambojrajat7500@gmail.com>
2025-07-09 12:52:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
   console.log(`FinSureTech app running at http://localhost:${port}`);
 });
+
+const client = require('prom-client');
+
+// Collect default system metrics (CPU, memory, etc.)
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+// Expose /metrics endpoint
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
+});
